8e74b45e8f3d4452d72ceb746b7ba11f170bfada
aprxty3 <ajiprxty3@gmail.com>
2023-05-09 20:41:49 +0700

    improve ui and set function on chat screen

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,4 +1,10 @@
+import 'package:chat_apps_with_flutter_supabase/utils/chat_bar.dart';
 import 'package:flutter/material.dart';
+
+import '../models/message_model.dart';
+import '../models/profile_model.dart';
+import '../utils/chat_bubble.dart';
+import '../utils/constans.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -14,10 +20,71 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  late final Stream<List<Message>> _messagesStream;
+  final Map<String, Profile> _profileCache = {};
+
+  @override
+  void initState() {
+    final myUserId = supabase.auth.currentUser!.id;
+    _messagesStream = supabase
+        .from('messages')
+        .stream(primaryKey: ['id'])
+        .order('created_at')
+        .map((maps) => maps
+            .map((map) => Message.fromMap(map: map, myUserId: myUserId))
+            .toList());
+    super.initState();
+  }
+
+  Future<void> _loadProfileCache(String profileId) async {
+    if (_profileCache[profileId] != null) {
+      return;
+    }
+    final data =
+        await supabase.from('profiles').select().eq('id', profileId).single();
+    final profile = Profile.fromMap(data);
+    setState(() {
+      _profileCache[profileId] = profile;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chat')),
+      body: StreamBuilder<List<Message>>(
+        stream: _messagesStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final messages = snapshot.data!;
+            return Column(
+              children: [
+                Expanded(
+                  child: messages.isEmpty
+                      ? const Center(
+                          child: Text('Memulai percakapan'),
+                        )
+                      : ListView.builder(
+                          reverse: true,
+                          itemCount: messages.length,
+                          itemBuilder: (context, index) {
+                            final message = messages[index];
+                            _loadProfileCache(message.profileId);
+                            return ChatBubble(
+                              message: message,
+                              profile: _profileCache[message.profileId],
+                            );
+                          },
+                        ),
+                ),
+                const ChatBar(),
+              ],
+            );
+          } else {
+            return preloader;
+          }
+        },
+      ),
     );
   }
 }
