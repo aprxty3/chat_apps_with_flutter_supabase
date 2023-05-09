efe73e969199ef13ba46a984097b26f62ab05c0c
aprxty3 <ajiprxty3@gmail.com>
2023-05-09 14:55:25 +0700

    add token

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
+import 'package:chat_apps_with_flutter_supabase/utils/constans.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://iuaaimeboychzvqrpvyw.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YWFpbWVib3ljaHp2cXJwdnl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2MTUxMzIsImV4cCI6MTk5OTE5MTEzMn0.1xQor0j2dIiIA_DTzicWhNzHIhOFmCoqTwOL1JGG2F4',
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'My Chat App',
+        theme: appTheme,
+        home: const Scaffold(
+          body: Center(
+            child: Text('Hello World'),
+          ),
+        ));
   }
 }
